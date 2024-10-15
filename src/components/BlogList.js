ef9d3109d17a6fa4411e4e67cadf8c0722af1883
widk0419 <74219184+widk0419@users.noreply.github.com>
2024-10-15 17:56:02 +0900

    pagination 진행 중 수정해야할 부분 있음

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Card from "../components/Card";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import LoadingSpinner from "../components/LoadingSpinner";
 import { bool } from "prop-types";
-import PagiNation from "./Pagination";
+import Pagination from "./Pagination";
 
 const BlogList = ({isAdmin = false}) => {
 
     const history = useHistory();
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const pageParam = params.get('page');
     const [posts, setPosts] = useState([]) ;
     const [loading, setLoading] = useState(true);
     const [currentPage, setCurrentPage] = useState(1);
@@ -20,8 +23,11 @@ const BlogList = ({isAdmin = false}) => {
         setNumberOfPages(Math.ceil(numberOfPosts/limit));
     }, [numberOfPosts]);
 
+    const onClickPageButton = (page) => {
+        history.push(`${location.pathname}?page=${page}`);
+    }
+
     const getPosts = (page = 1) => {
-        setCurrentPage(page);
         let params = {
             _page: page,
             _limit: limit,
@@ -36,12 +42,16 @@ const BlogList = ({isAdmin = false}) => {
         axios.get(`http://localhost:3001/posts`,{ 
             params: params
         }).then((res) => {
-            console.log(res.data.length);
+            console.log('post갯수:',res.data.length);
             setNumberOfPosts(res.headers['x-total-count']);
             setPosts(res.data);
             setLoading(false);
         })
     }
+    useEffect(() => {
+        setCurrentPage(parseInt(pageParam) || 1);
+        getPosts(parseInt(pageParam) || 1);
+    }, [pageParam]);
 
     const deleteBlog = (e, id) => {
         e.stopPropagation();        
@@ -50,10 +60,6 @@ const BlogList = ({isAdmin = false}) => {
         });
     };
 
-    useEffect(() => {
-        getPosts();
-    }, []);
- 
 
     if(loading){
         return (
@@ -94,10 +100,10 @@ const BlogList = ({isAdmin = false}) => {
     return (
         <div>
             {renderBlogList()}
-            <PagiNation 
+            <Pagination 
                 currentPage={currentPage} 
                 numberOfPages={numberOfPages}
-                onClick={getPosts}
+                onClick={onClickPageButton}
                 limit
             />
         </div>
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,42 +1,55 @@
-import propTypes, { number } from 'prop-types';
+import propTypes from 'prop-types';
 
-const PagiNation = ({currentPage = 1, numberOfPages, onClick, limit=5}) => {
+const Pagination = ({currentPage = 1, numberOfPages, onClick, limit = 5}) => {
+    const currentSet = Math.ceil(currentPage/limit);
+    const lastSet = Math.ceil(numberOfPages/limit);
+    const startPage = limit * (currentSet - 1) + 1;
+    console.log('currentPage',currentPage,'limit',limit,'numberOfPages',numberOfPages,'startPage',startPage);
+    const numberOfPageForSet = currentSet === lastSet ? numberOfPages%limit : limit
+
     return (
         <nav aria-label="Page navigation example">
             <ul className="pagination justify-content-center">
-                <li className="page-item disabled">
-                    <a className="page-link" href='#'>
+                {currentSet !== 1 && <li className="page-item">
+                    <div className="page-link" href='#'
+                        onClick={() => onClick(startPage - limit)}
+                    >
                         Previous
-                    </a>
-                </li>
-                {Array(numberOfPages).fill(1).map((value,index) => value + index )
+                    </div>
+                </li>}
+                {Array(numberOfPageForSet).fill(startPage)
+                .map((value, index) => value + index )
                 .map((pageNum) => {
                     return <li key={pageNum} className={`page-item ${currentPage === pageNum ? 'active' : '' } `}>
                         <div className="page-link cursor-pointer"
                             onClick = {(e) => {
                                 onClick(pageNum);
-                        }}>  
+                            }}>  
                             {pageNum}
                         </div>
                     </li>
                 })}
-                <li className="page-item">
-                    <a className="page-link" href="#">Next</a>
-                </li>
+                {currentSet !== lastSet && <li className="page-item">
+                    <div className="page-link cursor-pointer" href="#"
+                        onClick={() => onClick(startPage + limit)}
+                    >
+                        Next</div>
+                </li>}
             </ul>
         </nav>
     )
 }
 
-PagiNation.propTypes = {
+Pagination.propTypes = {
     currentPage: propTypes.number,
     numberOfPages: propTypes.number.isRequired,
-    onClick: propTypes.func.isRequired
+    onClick: propTypes.func.isRequired,
+    limit: propTypes.number
 };
 
-// PagiNation.defaultProps = {
+// Pagination.defaultProps = {
 //     currentPage: 1,
 //     limit: 5
 // }
 
-export default PagiNation;
+export default Pagination;
